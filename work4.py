2abee846beb18a0b9d618084ca3e3f562ed0ba2f
sinetta <sinettaraj28008@gmail.com>
2023-09-12 20:15:22 +1245

    adding files

---
+++ work4.py
@@ -0,0 +1,25 @@
+#  operations
+a=10
+a+=20
+print(a)
+b=20
+b-=20
+print(b)
+c=40
+print(c==40 and c<20)
+d=50
+print(d==40 or d>20)
+e=10
+print(not(e==10))
+e=20
+print(not(e==15))
+s="python"
+print("y" in s)
+print("s" in s)
+f="Hello"
+j="hello"
+print(f is j)
+print(f is not j)
+m= """if you want to shine like a
+      sun first burn like a sun"""
+print(m)
