0273c2bbc1c7b625ff3d8fb2aac3a7495d702f17
sinetta <sinettaraj28008@gmail.com>
2023-09-22 13:08:47 +0530

    text

--- loop.py
+++ loop.py
@@ -1,8 +1,12 @@
-a=[1,2,3,4,5,6]
-print(a)
-for i in a:
-    print(i)
+# a=[1,2,3,4,5,6]
+# print(a)
+# for i in a:
+#     print(i)
 
-b=int(input("enter a number:"))
-for i in range(1,11):
-     print(i,'x',b,'=',i*b)
+# b=int(input("enter a number:"))
+# for i in range(1,11):
+#      print(i,'x',b,'=',i*b)
+
+a=int(input())
+for i in range(a+1):
+    print(i*'*')
