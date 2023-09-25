7391ff375b44343be9a161c361bb3a06d37f5e09
sinetta <sinettaraj28008@gmail.com>
2023-09-25 13:30:31 +0530

    text

---
+++ loop3.py
@@ -0,0 +1,16 @@
+# a=int(input("enter a number:"))
+# for i in range(1,a):
+#     print(i*"*")
+# for j in range(a,0,-1):
+#     print(j*"*")
+        
+
+# for i in range(1,6):
+#    for j in range(1,i+1):
+#      print(j, end=' ')
+#    print(' ')
+
+for i in range (6,0,-1):
+    for j in range (i,0,-1):
+        print(j,end='')
+    print('')
