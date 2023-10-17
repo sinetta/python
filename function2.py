e30b2b6595ffde7b86119a2a90cd04091093fda9
sinetta <sinettaraj28008@gmail.com>
2023-10-17 18:06:51 +0530

    text

--- function2.py
+++ function2.py
@@ -8,7 +8,7 @@ while True:
     2)view account
     3)add balance
     4)withdraw balance
-        ''')
+    5)quit   ''')
     opt=int(input("enter your choice:"))
     if opt==1:
         name=input("enter your name:")
@@ -22,7 +22,13 @@ while True:
         bank["balance"]=balance
         new.append(bank.copy())
     elif opt==2:
-        print(new)
+        # print(new)
+        
+        for i in new:
+            print('='*10)
+            for key , value in i.items():
+                print(key,':',value)
+            print('='*10)
     elif opt==3:
         acc_no=int(input("enter the acc_no:"))
         amount=int(input("enter the amount:"))
@@ -46,9 +52,14 @@ while True:
         if acc_no in acc:
             for i in new:
                if i["account_no"]==acc_no:
-                   i["balance"]-=amount
+                    if i["balance"]>=amount:
+                       i["balance"]-=amount
+                    else:
+                        print("not possible")
         else:
             print("no account found")
+    elif opt==5:
+        exit()
     
 
             
