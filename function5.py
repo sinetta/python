8a7cf8f4056475ff797905d2cd66a82431d29939
sinetta <sinettaraj28008@gmail.com>
2023-10-28 14:12:18 +0530

    text

--- function5.py
+++ function5.py
@@ -2,6 +2,7 @@ student_details={}
 details=[]
 teacher=[]
 students=[]
+fees=12000
 while True:
     print('''
     1)enter details of student
@@ -9,7 +10,8 @@ while True:
     3)enter detaiils of teacher
     4)assign
     5)fees     
-    6)quit
+    6)incharge
+    7)display
     ''')
     choice=int(input("enter your choice:"))
     if choice==1:
@@ -22,8 +24,10 @@ while True:
         details.append(student_details.copy())
     elif choice==2:
         for i in details:
+            print('-'*15)
             for j,k in i.items():
                 print(j,":",k)
+            print('-'*15)
     elif choice==3:
         name=input("enter the name:")
         teacher.append(name)
@@ -47,9 +51,29 @@ while True:
         if st_name and cls in students:
             for i in details:
                 if st_name==i["name"] and cls==i["class"]:
-                    i["fees"]=amount
+                    i["paid"]=amount
+                    i["balance"]=fees-amount
+                    if i["balance"]==0:
+                        i["balance"]="fees completed"
             print(details)
-    
+        else:
+            print("student not found")
+
+    elif choice==6:
+        tea_name=input("enter teacher's name:")
+        if tea_name in teacher:
+            for i in details:
+                if i["teacher"]==tea_name:
+                    print(i["name"])   
+        else:
+            print("not found")
+    elif choice==7:
+        for i in details:
+            print('-'*15)
+            print("name :",i["name"])
+            print("paid fees :",i["paid"])
+            print("balance :",i["balance"])
+            print('-'*15)
 
         
         
