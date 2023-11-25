c0bc05bbecf6ebd76b8fc4af92adadf344edb7d7
sinetta <sinettaraj28008@gmail.com>
2023-11-25 17:11:16 +0530

    add

---
+++ rithu/education/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from . models import *
+# Register your models here.
+
+class Department_display(admin.ModelAdmin):
+    list_display=['name']
+
+class Batch_display(admin.ModelAdmin):
+    list_display=['batch']
+
+class Teacher_display(admin.ModelAdmin):
+    list_display=['name','dept']    
+
+class Student_display(admin.ModelAdmin):
+    list_display=['name','batch','teacher']
+
+admin.site.register(Department,Department_display)
+admin.site.register(Batch,Batch_display)
+admin.site.register(Teacher,Teacher_display)
+admin.site.register(Student,Student_display)
