5d4e7580dc3c96484aff3f99a883ab4e2dbbea62
sinetta <sinettaraj28008@gmail.com>
2024-01-10 15:44:47 +0530

    add

--- project/roundplanet/tourism/urls.py
+++ project/roundplanet/tourism/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     
     path('',views.index,name='index'),
     path('about/',views.about,name='about'),
-    path('packages/',views.packages,name='package'),
+    path('packages/',views.packages,name='packages'),
     path('news/',views.news,name='news'),
     path('gallery/',views.gallery,name='gallery'),
     path('contact/',views.contact,name='contact'),
